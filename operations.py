8335e83a2e644b51dda6b0622faf21f05497da56
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-08 00:45:04 +0000

    Load 16 bit immediate

--- operations.py
+++ operations.py
@@ -1,20 +1,26 @@
 
-def nop(cpu): # 0x00 NOP
+def nop(cpu):                   # 0x00 NOP
     cpu.PC += 1
     return 4
 
-def ld_de_d16(cpu,mem): # 0x01 LD BC, d16
+def ld_bc_d16(cpu,mem):         # 0x01 LD BC, d16
+    cpu.B = mem[cpu.PC+2]
+    cpu.C = mem[cpu.PC+1]
+    cpu.PC += 3
+    return 12
+
+def stop(cpu):                  # 0x10 stop
+    # TODO
+    cpu.PC += 2
+    return 4
+
+def ld_de_d16(cpu,mem):         # 0x11 LD DE, d16
     cpu.D = mem[cpu.PC+2]
     cpu.E = mem[cpu.PC+1]
     cpu.PC += 3
     return 12
 
-def stop(cpu): # 0x10 stop
-    # TODO
-    cpu.PC += 2
-    return 4
-
-def jr_nz_r8(cpu,mem): # 0x20 JR NZ, r8
+def jr_nz_r8(cpu,mem):          # 0x20 JR NZ, r8
     if cpu.F & (1 << 7):
         cpu.PC += 1
         return 8
@@ -22,7 +28,13 @@ def jr_nz_r8(cpu,mem): # 0x20 JR NZ, r8
     cpu.PC += mem[cpu.PC+1]
     return 12
 
-def jr_nc_r8(cpu,mem): # 0x30 JR NZ, r8
+def ld_hl_d16(cpu,mem):         # 0x21 LD HL, d16
+    cpu.H = mem[cpu.PC+2]
+    cpu.L = mem[cpu.PC+1]
+    cpu.PC += 3
+    return 12
+
+def jr_nc_r8(cpu,mem):          # 0x30 JR NZ, r8
     if cpu.F & (1 << 4):
         cpu.PC += 1
         return 8
@@ -30,12 +42,12 @@ def jr_nc_r8(cpu,mem): # 0x30 JR NZ, r8
     cpu.PC += mem[cpu.PC+1]
     return 12
 
-def ld_sp_d16(cpu,mem): # 0x31 LD SP,d16
+def ld_sp_d16(cpu,mem):         # 0x31 LD SP,d16
     cpu.SP = (mem[cpu.PC+2] << 8) + mem[cpu.PC+1]
     cpu.PC += 3
     return 12
 
-def XOR_A(cpu): # 0xaf XOR A
+def XOR_A(cpu):                 # 0xaf XOR A
     cpu.A ^= cpu.A
     if cpu.A: cpu.F |= 1 << 7
     else: cpu.F &= ~(1 << 7) & 127
@@ -43,11 +55,13 @@ def XOR_A(cpu): # 0xaf XOR A
 
 op_codes = {
     0x00: lambda cpu: nop(cpu),
-    0x01: lambda cpu: ld_de_d16(cpu,cpu.M),
+    0x01: lambda cpu: ld_bc_d16(cpu,cpu.M),
     
     0x10: lambda cpu: stop(cpu),
+    0x11: lambda cpu: ld_de_d16(cpu,cpu.M),
     
     0x20: lambda cpu: jr_nz_r8(cpu,cpu.M),
+    0x21: lambda cpu: ld_hl_d16(cpu,cpu.M),
     
     0x30: lambda cpu: jr_nc_r8(cpu,cpu.M),
     0x31: lambda cpu: ld_sp_d16(cpu,cpu.M),
