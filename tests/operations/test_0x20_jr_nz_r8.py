a119a4eb5a417242e689fe62a2c6ee9e622ed1b8
Meliv <18529851+Meliv@users.noreply.github.com>
2024-12-09 00:44:46 +0000

    Ok I really need to go to bed

--- tests/operations/test_0x20_jr_nz_r8.py
+++ tests/operations/test_0x20_jr_nz_r8.py
@@ -37,3 +37,37 @@ class JR_NZ_R8_Test(unittest.TestCase):
         self.assertEqual(cpu.SP, 0)
         
         self.assertTrue(cpu.M, not all([]))
+        
+    @given(integers(min_value=0x00, max_value=0xff))
+    def test_jr_nz_r8_NZ(self, r8):
+
+        memory = [0x20, r8]
+        cpu = CPU(memory)
+        
+        cpu.F_Z = 0
+        
+        cycles = jr_nz_r8(cpu, cpu.M)
+
+        self.assertEqual(cycles, 12)
+        
+        self.assertEqual(cpu.A, 0)
+        self.assertEqual(cpu.F_Z, 0)
+        self.assertEqual(cpu.F_N, 0)
+        self.assertEqual(cpu.F_H, 0)
+        self.assertEqual(cpu.F_C, 0)
+        
+        self.assertEqual(cpu.B, 0)
+        self.assertEqual(cpu.C, 0)
+        self.assertEqual(cpu.D, 0)
+        self.assertEqual(cpu.E, 0)
+        self.assertEqual(cpu.H, 0)
+        self.assertEqual(cpu.L, 0)
+        
+        self.assertEqual(cpu.BC, 0)
+        self.assertEqual(cpu.DE, 0)
+        self.assertEqual(cpu.HL, 0)
+        
+        self.assertEqual(cpu.PC, r8)
+        self.assertEqual(cpu.SP, 0)
+        
+        self.assertEqual(cpu.M, memory)
